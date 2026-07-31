58b79d478286a077094c7d46dcbad9b537f96c28
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-07-31 11:23:35 -0300

    Programar primera bocha
    -	Declarar tabla bocha
    -	Declarar tabla baranda
    -	Inicializar cuerpo, forma y acople de bocha
    -	Dibujar bocha

--- main.lua
+++ main.lua
@@ -1,15 +1,36 @@
+baranda = {}
+bocha = {}
+
+
 function love.load()
     
     love.physics.setMeter(64)
-    world = love.physics.newWorld(0, 9.81*64, true)
+    world = love.physics.newWorld(0, 1*64, true)
 
-    cuerpo = love.physics.newBody(world, 650/2, 650-25)
-    forma  = love.physics.newRectangleShape(650, 50)
-    acople = love.physics.newFixture(cuerpo, forma)
+    baranda.cuerpo = love.physics.newBody(world, 650/2, 650-25)
+    baranda.forma  = love.physics.newRectangleShape(650, 50)
+    baranda.acople = love.physics.newFixture(baranda.cuerpo, baranda.forma)
+
+    bocha.cuerpo = love.physics.newBody(world, 650/2, 650/2,"dynamic")
+    bocha.forma = love.physics.newCircleShape(20)
+    bocha.acople = love.physics.newFixture(bocha.cuerpo, bocha.forma)
+    bocha.sprite = love.graphics.newImage("bola.png")
 
     love.window.setMode(650, 650)
 end
 
+function love.update(dt)
+  world:update(dt) 
+end
+
 function love.draw()
-    love.graphics.polygon("fill", cuerpo:getWorldPoints(forma:getPoints()))
+    love.graphics.polygon("fill", baranda.cuerpo:getWorldPoints(baranda.forma:getPoints()))
+    love.graphics.draw( bocha.sprite,
+                        bocha.cuerpo:getX(),
+                        bocha.cuerpo:getY(),
+                        0, 
+                        0.075,
+                        0.075, 
+                        256,
+                        256)
 end
