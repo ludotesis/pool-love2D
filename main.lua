4b58dc8cfb1addb749e7ac7859385b71ba57b57f
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-07-31 11:37:58 -0300

    Programar mover bocha
    -	Detectar tecla espacio
    -	Aplicar movimiento lineal a bocha
    -  Programar Segunda Baranda

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-baranda = {}
+baranda  = {}
+baranda2 = {}
 bocha = {}
 
 
@@ -11,6 +12,10 @@ function love.load()
     baranda.forma  = love.physics.newRectangleShape(650, 50)
     baranda.acople = love.physics.newFixture(baranda.cuerpo, baranda.forma)
 
+    baranda2.cuerpo = love.physics.newBody(world, 650/2, 0)
+    baranda2.forma  = love.physics.newRectangleShape(650, 50)
+    baranda2.acople = love.physics.newFixture(baranda2.cuerpo, baranda2.forma)
+
     bocha.cuerpo = love.physics.newBody(world, 650/2, 650/2,"dynamic")
     bocha.forma = love.physics.newCircleShape(20)
     bocha.acople = love.physics.newFixture(bocha.cuerpo, bocha.forma)
@@ -19,12 +24,22 @@ function love.load()
     love.window.setMode(650, 650)
 end
 
+function love.keypressed(key, scancode, isrepeat)
+    if key == "space" then
+        bocha.cuerpo:applyLinearImpulse(0,-1000)
+    end
+end
+
 function love.update(dt)
   world:update(dt) 
 end
 
 function love.draw()
+    love.graphics.setColor(0.6, 0.4, 0.2)
     love.graphics.polygon("fill", baranda.cuerpo:getWorldPoints(baranda.forma:getPoints()))
+    love.graphics.polygon("fill", baranda2.cuerpo:getWorldPoints(baranda.forma:getPoints()))
+    
+    love.graphics.setColor(1, 1, 1)
     love.graphics.draw( bocha.sprite,
                         bocha.cuerpo:getX(),
                         bocha.cuerpo:getY(),
